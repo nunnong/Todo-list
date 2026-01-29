ba5f897aeb723d821b21c9aa0bebccc93b8c6b2c
yunyoung <juyy99@gmail.com>
2026-01-29 15:45:02 +0900

    feat: AddButton 컴포넌트 추가

---
+++ src/components/AddButton/AddButton.tsx
@@ -0,0 +1,16 @@
+import PlusGrayIcon from "@public/icons/plus_gray.svg";
+
+interface AddButtonProps {
+  onClick?: () => void;
+}
+
+export default function AddButton({ onClick }: AddButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="w-16 h-16 rounded-full bg-slate-200 flex items-center justify-center cursor-pointer"
+    >
+      <PlusGrayIcon className="w-6 h-6" />
+    </button>
+  );
+}
